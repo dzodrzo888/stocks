87bb15adfa204df7568690355e32b7e5789506f9
dzodrzo888 <jirkaapesek@seznam.cz>
2025-01-26 20:44:13 +0100

    Added the first base and index page

--- stocks/stocks_app/models.py
+++ stocks/stocks_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stocks = models.ManyToManyField(Stock)
+        
--- stocks/stocks_app/views.py
+++ stocks/stocks_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def home_view(request):
+    context = {
+        'is_logged_in': request.user.is_authenticated,
+        'username': request.user.username if request.user.is_authenticated else None
+    }
+    return render(request, 'index.html', context=context)
